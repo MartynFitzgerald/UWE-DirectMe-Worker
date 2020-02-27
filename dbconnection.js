5108a68250dd4845f2c6a989637502e25a933e23
m4-fitzgerald <martyn2.fitzgerald@live.uwe.ac.uk>
2020-02-27 15:58:32 +0000

    Adding the check of existing car parks and DB.

---
+++ dbconnection.js
@@ -0,0 +1,22 @@
+/*=============================================================================
+|      Editors:  Martyn Fitzgerald - 16025948
+|
+|  Module Code:  UFCFR4-45-3
+| Module Title:  Computing Project
+|
+|   Instructor:  Paul Raynor
+|     Due Date:  30/03/2019
+|
+|    File Name:  dbconnection.js  
+|  Description:  This is the logic behind the MySQL database connection which
+|                includes the login details for the database
+*===========================================================================*/
+var mysql = require('mysql');
+
+exports.connection = mysql.createConnection({
+    host     : process.env.RDS_HOSTNAME,
+    user     : process.env.RDS_USERNAME,
+    password : process.env.RDS_PASSWORD,
+    port     : process.env.RDS_PORT,
+    database : 'parking'
+  });
--- index.js
+++ index.js
@@ -7,30 +7,24 @@
 |   Instructor:  Paul Raynor
 |     Due Date:  30/03/2019
 |
-|    File Name:  scraper_worker/index.js  
-|  Description:  This is the worker that will be fetching data from the Google's
-|                API and INSERT it into the database.
+|    File Name:  index.js  
+|  Description:  This is the worker that will be fetching data of car parks
+|                from the Google's API and INSERT it into the database.
 *===========================================================================*/
 var request = require("request");
-var mysql = require('mysql');
 var moment = require('moment');
+var dbController = require('./dbconnection');
 
-function getDatabaseConnection() {
-  var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'parking'
-  });
-  return connection;
-}
+var scrapingLocations = [];
+var carParks = [];
 
+/* 
+  A function that selects all scraping locations stored within the database and returns the result.
+*/
 function getScrapingLocations(callback) {
-  var sql = "select * from scraping_location;";
+  var sql = "SELECT * FROM scraping_location;";
 
-  var connection = getDatabaseConnection();
-
-  connection.query(sql, function(error, result) {
+  dbController.connection.query(sql, function(error, result) {
     if (error) {
       return callback(error);
     }
@@ -38,12 +32,12 @@ function getScrapingLocations(callback) {
     return callback(null, result);
   });
 }
-var scrapingLocations = [];
-var carParks = [];
 
+/* 
+  A function that request data from google's api and loops through the pages. 
+*/
 function requestDataFromGoogleApi(url, nextPageToken = null, callback) {
   var currentUrl = url;
-  console.log("Test 1" , currentUrl);
 
   if (nextPageToken) {
     currentUrl += "&pagetoken=" + nextPageToken;
@@ -73,6 +67,9 @@ function requestDataFromGoogleApi(url, nextPageToken = null, callback) {
   });
 }
 
+/* 
+  A function that inserts a car park to the databases. 
+*/
 function carparksInsert(lat, lng, radius, scrapingLocationId) {
   // Create The URL to the API 
   var url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?type=parking&radius=${radius}&location=${lat},${lng}&key=AIzaSyBeMKzk8ZpyU2Hk_lrVmlO-Ggq1tQqtYsM`
@@ -83,38 +80,62 @@ function carparksInsert(lat, lng, radius, scrapingLocationId) {
       process.exit();
     }
 
-    if (result.length > 0) { //If there are results, then...
+    if (result.length > 0) {
       for(var i = 0; i < result.length; i++)
       {
-        var carpark = result[i];
-
-        var externalId = carpark.id;
+        //Assigning key values to be inputted into the sql statement
+        var externalId = result[i].id;
         var externalProvider = "Google"
-        var name = carpark.name;
-        var latitude = carpark.geometry.location.lat;
-        var longitude = carpark.geometry.location.lng;
+        var name = result[i].name;
+        var latitude = result[i].geometry.location.lat;
+        var longitude = result[i].geometry.location.lng;
         // Using moment libary to allow be able to add this into MySQL timestamp format 
         var lastUpdatedAt = moment(Date.now()).format('YYYY-MM-DD HH:mm:ss');
 
-        //var rating = carpark.rating;
-        //var userRatingsTotal = carpark.user_ratings_total;
-        //var photos = null;
+        var sql = `SELECT * FROM car_park WHERE external_id = '${externalId}';`;
 
-        var sql = `INSERT IGNORE INTO car_park (scraping_location_id, external_id, external_provider, name, latitude, longitude, last_updated_at) VALUES ('${scrapingLocationId}', '${externalId}','${externalProvider}','${name}','${latitude}', '${longitude}', '${lastUpdatedAt}');`;
-       
-        var connection = getDatabaseConnection();
-        //TODO: before returning to the user, save all new records to the database
-        connection.query(sql, function(error, result, fields) {
+        dbController.connection.query(sql, function(error, result, fields) {
           if (error) {
-            console.error('Failed to get request data from google api', error);
-            process.exit();
+            return callback(error);
+          }
+
+          if (result.length > 0) {
+            if ((result[0].name =! name) || (result[0].latitude =! latitude) || (result[0].longitude =! longitude))
+            {
+              //Update car park with the new values
+              var sql = `UPDATE car_park SET name = '${name}', latitude = '${latitude}', longitude = '${longitude}', last_updated_at = '${externalId}' WHERE external_id = '${externalId}';`
+
+              dbController.connection.query(sql, function(error, result, fields) {
+                if (error) {
+                  console.error(`Failed to update - '${externalId}'`, error);
+                  process.exit();
+                }
+              });
+            }
+          }
+          else
+          {
+            //Insert new car park
+            var sql = `INSERT IGNORE INTO car_park (scraping_location_id, external_id, external_provider, name, latitude, longitude, last_updated_at) VALUES ('${scrapingLocationId}', '${externalId}','${externalProvider}','${name}','${latitude}', '${longitude}', '${lastUpdatedAt}');`;
+          
+            dbController.connection.query(sql, function(error, result, fields) {
+              if (error) {
+                console.error(`Failed to insert - '${externalId}'`, error);
+                process.exit();
+              }
+            });
           }
         });
+
+        
       }
     }
   });
 }
 
+/* 
+  The initializer of getting scraping locations.
+*/
 getScrapingLocations(function(error, result) {
   if (error) {
     console.error('Failed to get scraping locations', error);
